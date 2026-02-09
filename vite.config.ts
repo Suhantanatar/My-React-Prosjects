8a64187c03fa727dbc3f6f3a7608b41e4fd2745c
BAS1C010 <sb.tanatar@gmail.com>
2026-02-09 10:38:10 +0100

    Fix package.json and prepare for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace with the name of your GitHub repo:
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/My-React-Prosjects/', // 👈 must match your repo name
+});
